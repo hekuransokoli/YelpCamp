86fb133bafaf2d4c60003077c11cdef388df1d32
Hekuran Sokoli <hekuran.sokoli@student.uni-pr.edu>
2024-07-19 16:16:31 +0200

    ready to try deploying

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo');
 // const dbUrl = process.env.DB_URL;
 
-const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 mongoose.connect(dbUrl, {
 });
@@ -46,11 +46,13 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(mongoSanitize());
 
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+
 const store = MongoStore.create({
     mongoUrl: dbUrl,
     touchAfter: 24 * 60 * 60,
     crypto: {
-        secret: 'thisshouldbeabettersecret!'
+        secret,
     }
 });
 
@@ -61,7 +63,7 @@ store.on("error", function (e) {
 const sessionConfig = {
     store,
     name: 'session',
-    secret: 'thisshouldbeabettersecret!',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
